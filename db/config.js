e8c57616e8e051fb8d76a7b50637bbe2b470740f
Kevin Valencia Cruz <kev_val64@hotmail.com>
2022-03-03 12:48:11 -0600

    Migraciones y SQL - Se agrega imagen para SQL - Se agrega conf para correr migraciones

--- db/config.js
+++ db/config.js
@@ -1,19 +1,16 @@
-const { Sequelize } = require('sequelize');
-
 const { config } = require('../config/config');
-const setUpModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
-  logging: true,
-});
-
-setUpModels(sequelize);
-
-sequelize.sync();
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'mysql',
+  },
+  production: {
+    url: URI,
+    dialect: 'mysql',
+  },
+};

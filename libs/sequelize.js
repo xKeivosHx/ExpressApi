e438f838940c653b50ae7bcf2987d73e67819b8d
Kevin Valencia Cruz <kev_val64@hotmail.com>
2022-03-04 08:42:42 -0600

    Cambios - Se crean migraciones - Se crean comandos para correr migraciones - Se crean archivos para customer

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -14,6 +14,6 @@ const sequelize = new Sequelize(URI, {
 
 setUpModels(sequelize);
 
-sequelize.sync();
+//sequelize.sync();
 
 module.exports = sequelize;
---
+++ services/customer.service.js
@@ -0,0 +1,42 @@
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
+
+class CustomerService {
+  constructor() {}
+
+  async find() {
+    const rta = await models.Customer.findAll({
+      include: ['user'],
+    });
+    return rta;
+  }
+
+  async findOne(id) {
+    const customer = await models.Customer.findByPk(id);
+    if (!customer) {
+      throw boom.notFound('customer not found');
+    }
+    return customer;
+  }
+
+  async create(data) {
+    const newCustomer = await models.Customer.create(data, {
+      include: ['user'],
+    });
+    return newCustomer;
+  }
+
+  async update(id, changes) {
+    const customer = await this.findOne(id);
+    const rta = await customer.update(changes);
+    return rta;
+  }
+
+  async delete(id) {
+    const customer = await this.findOne(id);
+    await customer.destroy();
+    return { rta: true };
+  }
+}
+
+module.exports = CustomerService;

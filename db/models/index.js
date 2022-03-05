63053e24967c3012485ea636b4d306ba50f3c5ae
Kevin Valencia Cruz <kev_val64@hotmail.com>
2022-03-04 23:18:51 -0600

    Ordenes de Compra - Se sube modelo, schema, service y router para ordenes y se dan de alta en el index de la carperta models.

--- db/models/index.js
+++ db/models/index.js
@@ -2,17 +2,20 @@ const { User, UserSchema } = require('./user.model');
 const { Product, ProductSchema } = require('./product.model');
 const { Category, CategorySchema } = require('./category.model');
 const { Customer, CustomerSchema } = require('./customer.model');
+const { Order, OrderSchema } = require('./order.model');
 
 function setUpModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
+  Order.init(OrderSchema, Order.config(sequelize));
 
   User.associate(sequelize.models);
   Category.associate(sequelize.models);
   Product.associate(sequelize.models);
   Customer.associate(sequelize.models);
+  Order.associate(sequelize.models);
 }
 
 module.exports = setUpModels;

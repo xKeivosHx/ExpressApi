f6b951785065bddfa797b3d1e5ee6c364c6d4043
Kevin Valencia Cruz <kev_val64@hotmail.com>
2022-03-09 19:14:27 -0600

    SCHEMA FIX

--- db/migrations/4.-orders.js
+++ db/migrations/4.-orders.js
@@ -1,10 +1,35 @@
 'use strict';
 
-const { OrderSchema, ORDER_TABLE } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
 
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        References: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
   },
 
   down: async (queryInterface) => {
